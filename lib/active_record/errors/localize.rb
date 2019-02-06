c61df980f1428d4c62ac126c9d0c9f7113b70250
松本和也 <matsumotokazuya7@gmail.com>
2019-02-06 15:06:29 +0900

    initialize

---
+++ lib/active_record/errors/localize.rb
@@ -0,0 +1,10 @@
+require "active_record/errors/localize/version"
+
+module ActiveRecord
+  module Errors
+    module Localize
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/active_record/errors/localize/version.rb
@@ -0,0 +1,7 @@
+module ActiveRecord
+  module Errors
+    module Localize
+      VERSION = "0.1.0"
+    end
+  end
+end
